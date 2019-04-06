4258e85f50f5e8f2ea07e8344a78b5420e9b70f4
LENOVO <zhihi455@126.com>
2019-04-06 12:59:08 +0800

    add swagger2

--- src/main/java/com/megvii/demo/controller/StudentController.java
+++ src/main/java/com/megvii/demo/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.megvii.demo.controller;
 
 import com.megvii.demo.domain.Student;
 import com.megvii.demo.service.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(description = "学员接口")
 @RestController
 @RequestMapping("/student")
 public class StudentController {
@@ -17,6 +20,7 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    @ApiOperation(value = "查询所有学员", notes = "学员列表")
     @ResponseBody
     @GetMapping("/all/list")
     public List<Student> listAll() {
--- src/main/java/com/megvii/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/megvii/demo/service/impl/StudentServiceImpl.java
@@ -3,12 +3,14 @@ package com.megvii.demo.service.impl;
 import com.megvii.demo.dao.StudentMapper;
 import com.megvii.demo.domain.Student;
 import com.megvii.demo.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class StudentServiceImpl implements StudentService {
 
     @Autowired
@@ -16,6 +18,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Student> listAll() {
+        log.info("Invoke listAll...");
         return studentMapper.listAll();
     }
 
